f337a3094192bb8ca7ffc0cb24d60ffb0f1218e8
pakorndiode <pakorn_sillapaprapha@cmu.ac.th>
2021-08-01 20:01:58 +0700

    up data in index2 and write index === 0

--- script.js
+++ script.js
@@ -6,16 +6,17 @@ const all_Product = async () =>{
         document.getElementById('allproduct').innerHTML = respone.data.map(
             (product) =>`
             <div class="card m-2" style="width: 18rem;">
-                <a href="index2.html"><img src="${product.prdImageUrl}" class="card-img-top" alt="${product.prdname}"></a>
+                <img src="${product.prdImageUrl}" class="card-img-top" alt="${product.prdname}" 
+                    onclick="location.href = 'index2.html?id=${product.id}'" style="cursor: pointer;">
                 <div class="card-body">
                     <h5 class="card-title">${product.prdname}</h5>
                     <div class="prices">
                     <p style="display: inline;" class="card-text">${product.prdPrice}</p>
-                    <s style="display: inline;" class="card-text-sell">$67.86</s>
+                    <s style="display: inline;" class="card-text-sell">${product.prdPrice}</s>
                     </div>
                 </div>
                 <div class="d-flex justify-content-end">
-                <button class="col-4 btn btn-outline-dark m-2" onclick="btn_Add(${product})">Add</button>
+                <button class="col-4 btn btn-outline-dark m-2" onclick="location.href = 'basket.html?id=${product.id}'">Add</button>
                 </div>
             </div>
             `
@@ -50,6 +51,72 @@ const slide = async () => {
 
 slide()
 
-function btn_Add(product){
-    console.log(product)
+/////////////////////////////////////////////////Index2//////////////////////////////////////////////////
+
+let prdid = new URLSearchParams(window.location.search).get("id");
+console.log('id',prdid)
+
+const product_show = async () =>{
+    try {
+        const respone = await axios.get(
+            `https://6102d7aa79ed680017482359.mockapi.io/productdetail?id=${prdid}`
+        )
+        document.getElementById('productTitle').innerHTML = respone.data.map(
+            (product,index) =>
+            {if (index === 0) {
+                return `${product.prdname}`
+            }}    
+        ).join('')
+        document.getElementById('productDetails').innerHTML = respone.data.map(
+            (product,index) =>
+            {if (index === 0) {
+            return`    
+            <img class="col-lg-6 col-sm-12" src="${product.prdImageUrl}" alt="" height="80%" width="100%">
+            <div class="col-lg-6 col-sm-12 d-flex justify-content-center ">
+                <div>
+                    <label id="productName" style="font-size: 2rem;">${product.prdname}</label>
+                    <br><label id="price" class="my-4" style="color: #B02B2B; font-size: 2rem;">${product.prdPrice} THB</label>
+                    <div>
+                        <label class="my-2" style="font-size: 1em; color: #646464;">Size</label>
+                        <select class="form-select my-2" aria-label="Default select example" id="prdSize">
+                            <option selected>Please Select</option>
+                            ${product.prdSize.map((value) =>{
+                                return `<option value="${value}">${value}</option>`
+                            })}
+                        </select>
+                        <div class="d-grid gap-2">
+                            <button type="button" class="btn btn-dark my-2" onclick="location.href = 'basket.html?id=${product.id}'">Add To Bag</button>
+                        </div>
+                        <div style="margin-top: 4rem;">
+                            <label style="font-size: 1.75rem; margin-bottom: 1rem;">Product Details</label>
+                            <label class="lh-base" style="font-size: 1.25rem;" id="details">
+                                ${product.txtDetail}
+                            </label>
+                        </div>
+                        <div class="d-grid gap-2 my-5">
+                            <button type="button" class="btn btn-light my-2">Show More Detail</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-basic">
+                <footer>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a href="#">Home</a></li>
+                        <li class="list-inline-item"><a href="#">Services</a></li>
+                        <li class="list-inline-item"><a href="#">About</a></li>
+                        <li class="list-inline-item"><a href="#">Terms</a></li>
+                        <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    </ul>
+                    <p class="copyright">Company Name © 2018</p>
+                </footer>
+            </div>
+        `   }
+        }).join('')
+    } catch (error) {
+        console.log('e',error)
+    }
 }
+
+product_show()
